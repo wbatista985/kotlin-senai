64b7b10d5aada39cf4b34adfe1d655155e110772
FIC <wbatista985@gmail.com>
2021-09-14 19:03:14 -0300

    single page
    app curso senai

--- app/src/main/java/com/example/langepage/MainActivity.kt
+++ app/src/main/java/com/example/langepage/MainActivity.kt
@@ -14,31 +14,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val carro : Button = findViewById(R.id.button)
+        carro.setOnClickListener { mostrarCarro() }
 
-        carro.setOnClickListener { rollDice() }
+        val moto : Button = findViewById(R.id.button2)
+        moto.setOnClickListener { mostrarMoto() }
+
+        val caminhao : Button = findViewById(R.id.button3)
+        caminhao.setOnClickListener { mostrarCaminhao() }
     }
 
-
-    private fun rollDice() {
-        val auto = Automoveis(3)
-        val diceRoll = auto.radar()
-
-        val diceImage: ImageView = findViewById(R.id.imageView)
-
-        when (diceRoll) {
-            1 -> diceImage.setImageResource(R.drawable.moto_1)
-            2 -> diceImage.setImageResource(R.drawable.carro_2)
-            3 -> diceImage.setImageResource(R.drawable.caminhao_3)
-
-        }
+    private fun mostrarMoto(){
+        val imagem: ImageView = findViewById(R.id.imageView)
+        imagem.setImageResource(R.drawable.moto_1)
     }
+    private fun mostrarCarro(){
+        val imagem: ImageView = findViewById(R.id.imageView)
+        imagem.setImageResource(R.drawable.carro_2)
+    }
+    private fun mostrarCaminhao(){
+        val imagem: ImageView = findViewById(R.id.imageView)
+        imagem.setImageResource(R.drawable.caminhao_3)
+    }
+
+//    private fun rollDice() {
+//        val auto = Automoveis(3)
+//        val diceRoll = auto.radar()
+//
+//        val diceImage: ImageView = findViewById(R.id.imageView)
+//
+//        when (diceRoll) {
+//            1 -> diceImage.setImageResource(R.drawable.moto_1)
+//            2 -> diceImage.setImageResource(R.drawable.carro_2)
+//            3 -> diceImage.setImageResource(R.drawable.caminhao_3)
+//
+//        }
+//    }
 }
 
 
-class Automoveis(private val tipes: Int) {
-
-
-    fun radar(): Int {
-        return (1..tipes).random()
-    }
-}
+//class Automoveis(private val tipes: Int) {
+//
+//
+//    fun radar(): Int {
+//        return (1..tipes).random()
+//    }
+//}
